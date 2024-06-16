33fb1fc98bcf5f4158a8705072d043d841abf85d
Mohit Raut <98951670+Mohitraut07@users.noreply.github.com>
2024-06-17 00:01:26 +0530

    Update App.js: Show available news
    API expired so local data is used to display to user

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   const [date, setDate] = useState('2023-11-13');
 
   useEffect(() => {
-    fetch(`https://newsapi.org/v2/everything?q=${category}&from=${date}&sortBy&apiKey=bda2acb6731d4cd9bec7d1e2bd2a1494`)
-      // fetch('articles.json')
+    //fetch(`https://newsapi.org/v2/everything?q=${category}&from=${date}&sortBy&apiKey=bda2acb6731d4cd9bec7d1e2bd2a1494`)
+      fetch('articles.json')
       .then(response => response.json())
       .then(data => {
         console.log(data);
